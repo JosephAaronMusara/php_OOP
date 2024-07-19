42b4171d794fda1b536d544c5fa7979ea133f6fc
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-19 15:57:52 +0200

    Multiplle objects of the same class

--- public/index.php
+++ public/index.php
@@ -4,11 +4,15 @@ declare(strict_types = 1);
 
 require_once 'Transactions.php';
 
-$transaction = (new Transactions(100,'Payment For Uncle Joe')) 
+$transaction1 = (new Transactions(100,'Payment For Uncle Joe')) 
                 -> addTax(8)
                 -> applyDiscount(10)
                 ->getAmount();
 
+$transaction2 = (new Transactions(100,'Payment For Mr B1k')) 
+                -> addTax(8)
+                -> applyDiscount(10)
+                ->getDescription();
 echo'<pre>';
-var_dump($transaction);
+var_dump($transaction1, $transaction2);
 echo'<pre />';
