625da32f9847201f5ed49e41af96ab5a5bbee905
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-19 15:45:45 +0200

    slight getters

--- public/Transactions.php
+++ public/Transactions.php
@@ -2,12 +2,32 @@
 
 class Transactions
 {
-    public float $amount;
-    public string $description;
+    private float $amount;
+    private string $description;
 
+    //NB: all methods that dont have access modifiers are assume to be public
     public function __construct(float $amount, string $description)
     {
         $this->amount = $amount;
         $this->description = $description;
+    }
+
+    public function addTax(float $rate)
+    {
+        $this->amount += $this->amount * $rate / 100;
+    }
+
+    public function applyDiscount(float $rate)
+    {
+        $this->amount -= $this->amount * $rate / 100;
+    }
+
+    public function getAmount(): float
+    {
+        return $this->amount;
+    }
+    public function getDescription(): string
+    {
+        return $this->description;
     }
 }
--- public/index.php
+++ public/index.php
@@ -4,6 +4,13 @@ declare(strict_types = 1);
 
 require_once 'Transactions.php';
 
-$transaction = new Transactions(23,'Payment For Uncle Joe');
+$transaction = new Transactions(100,'Payment For Uncle Joe');
 
-var_dump($transaction);
+$transaction -> addTax(8);
+$transaction -> applyDiscount(10);
+
+echo'<pre>';
+var_dump($transaction->getAmount());
+echo'<br />';
+var_dump( $transaction->getDescription());
+echo'<pre />';
